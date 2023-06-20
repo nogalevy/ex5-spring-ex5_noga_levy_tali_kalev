e27d73c30b4b2cd534870bc1009dc78448a5f662
nogalevy <nogalevy2@gmail.com>
2023-06-20 10:51:31 +0300

    connect to static folder

--- src/main/java/hac/WebConfig.java
+++ src/main/java/hac/WebConfig.java
@@ -1,2 +1,15 @@
-package hac;public class WebConfig {
+package hac;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
 }
