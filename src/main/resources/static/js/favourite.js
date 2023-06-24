b81de67c6914f7d92282ec3846be4e25b0578ab1
Tali Kalev <talkalev@gmail.com>
2023-06-24 16:45:30 +0300

    changed all buttons to icons

--- src/main/resources/static/js/favourite.js
+++ src/main/resources/static/js/favourite.js
@@ -1,7 +1,7 @@
 const cardsModule = (function () {
     let offset = 3; //TODO : const
 
-    const handleFlip = function(f,b) {
+    const handleFlip = function (f, b) {
         f.classList.toggle('flipped')
         b.classList.toggle('flipped')
     }
@@ -15,7 +15,10 @@ const cardsModule = (function () {
             },
             body: jokeId
         })
-            .then(response => {console.log("here2", response); return response.json()})
+            .then(response => {
+                console.log("here2", response);
+                return response.json()
+            })
             .then(deletedJokeId => {
                 console.log("here", deletedJokeId);
                 if (deletedJokeId !== null) {
@@ -36,11 +39,11 @@ const cardsModule = (function () {
             });
     }
 
-    const loadMoreFavourites = function (afterDelete = false){
+    const loadMoreFavourites = function (afterDelete = false) {
         let query = '?offset=' + offset;
-        if(afterDelete) query += '&limit=1';
+        if (afterDelete) query += '&limit=1';
         fetch('/favourites/get' + query)
-            .then(response => response.json() )
+            .then(response => response.json())
             .then(favourites => {
                 offset += favourites.length;
                 addCards(favourites);
@@ -50,11 +53,11 @@ const cardsModule = (function () {
             });
     }
 
-    const addCards = function (data){
-        data.forEach((fav)=>{
+    const addCards = function (data) {
+        data.forEach((fav) => {
             let newDiv = document.createElement('div');
             newDiv.id = 'card-' + fav.id;
-            newDiv.className =  "col card-con";
+            newDiv.className = "col card-con";
             newDiv.setAttribute("type", fav.type);
 
             let html = getCardHtml(fav);
@@ -62,54 +65,53 @@ const cardsModule = (function () {
             container.appendChild(newDiv);
             newDiv.innerHTML = html;
 
-            if(fav.type === 'twopart') addCardEvent(newDiv)
+            if (fav.type === 'twopart') addCardEvent(newDiv)
             addDeleteButtonEvent(newDiv);
         })
 
     }
 
-    const addCardEvent= function (card){
+    const addCardEvent = function (card) {
         const f = document.querySelector('#' + card.id + ' #front');
         const b = document.querySelector('#' + card.id + ' #back');
-        card.addEventListener('click', () => handleFlip(f,b))
+        card.addEventListener('click', () => handleFlip(f, b))
     }
 
-    const getCardHtml = function (fav){
+    const getCardHtml = function (fav) {
         return `<div>
                     <div class='card-container'>
-                    <div id='clickable' class='joke-card ${fav.type === 'twopart' ? 'flip-btn' : 'not-flip'}' >
-                        <div id="back" class="cardBack"> 
-                         <h3 id="backCardContent">${fav.delivery}</h3>
+                        <div id='clickable' class='joke-card ${fav.type === 'twopart' ? 'flip-btn' : 'not-flip'}' >
+                            <div id="back" class="cardBack"> 
+                                <h3 id="backCardContent">${fav.delivery}</h3>
+                            </div>
+                            <div id="front" class="cardFront">
+                              <h3 id="frontCardContent">${fav.type === 'twopart' ? fav.setup : fav.joke}</h3>
+                              <p class="flip-msg">Click to flip</p>
+                            </div>
                         </div>
-                      <div id="front" class="cardFront">
-                          <h3 id="frontCardContent">${fav.type === 'twopart' ? fav.setup : fav.joke }</h3>
-                          <p class="flip-msg">Click to flip</p>
-                         </div>
                     </div>
-              </div>
-            </div>
-            <i class="fa-solid fa-trash delete-btn" data-joke-id="${fav.id}"></i>`
+                    <div class="btn delete-btn" data-joke-id="${fav.id}">
+                         <i class="fa-solid fa-trash"></i>
+                    </div>
+                </div>`
     }
 
-    const deleteButtonEvent = function(button) {
+    const deleteButtonEvent = function (button) {
         const jokeId = button.getAttribute('data-joke-id');
         deleteJoke(jokeId);
     }
 
-    const addDeleteButtonEvent = function(card) {
+    const addDeleteButtonEvent = function (card) {
         const deleteButton = card.querySelector('.delete-btn');
         deleteButton.addEventListener('click', () => deleteButtonEvent(deleteButton));
     }
 
-    return{
+    return {
         loadMoreFavourites,
         addCardEvent,
         addDeleteButtonEvent
     }
 })();
-
-
-
 
 
 // --------------------------------------------------------------
